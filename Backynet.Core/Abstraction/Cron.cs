90b13f394e35a0e04d2e07f15174f3aa1d2e2ea4
nickofc <damiancz112@gmail.com>
2024-03-24 17:33:27 +0100

    feat: add cron minutes and hours fields

--- Backynet.Core/Abstraction/Cron.cs
+++ Backynet.Core/Abstraction/Cron.cs
@@ -2,21 +2,13 @@ namespace Backynet.Core.Abstraction;
 
 public readonly ref struct Cron
 {
-    private readonly int _second;
-    private readonly int _minute;
-    private readonly int _hour;
-    private readonly int _dayOfMonth;
-    private readonly int _month;
-    private readonly int _dayOfWeek;
+    private readonly MinutesField _minutesField;
+    private readonly HoursField _hoursField;
 
-    public Cron(int second, int minute, int hour, int dayOfMonth, int month, int dayOfWeek)
+    private Cron(MinutesField minutesField, HoursField hoursField)
     {
-        _second = second;
-        _minute = minute;
-        _hour = hour;
-        _dayOfMonth = dayOfMonth;
-        _month = month;
-        _dayOfWeek = dayOfWeek;
+        _minutesField = minutesField;
+        _hoursField = hoursField;
     }
 
     public static Cron Parse(ReadOnlySpan<char> input, CronType cronType = CronType.Standard)
@@ -26,42 +18,128 @@ public readonly ref struct Cron
             throw new NotSupportedException("Extended cron is not yet supported.");
         }
 
-        var second = 0;
-        var minute = 0;
-        var hour = 0;
-        var dayOfMonth = 0;
-        var month = 0;
-        var dayOfWeek = 0;
-        var position = 0;
+        // "0 22 * * 1-5"    = At 22:00 on every day-of-week from Monday through Friday.
+        // "23 0-20/2 * * *" = At minute 23 past every 2nd hour from 0 through 20.
+        // "5 4 * * sun"     = At 04:05 on Sunday.
+        // "0 0 1,15 * 3"    = At 00:00 on day-of-month 1 and 15 and on Wednesday.
 
-        return new Cron();
-    }
+        var reader = new CronReader(input);
 
-    private static ReadOnlySpan<char> GetNextSegment(ref ReadOnlySpan<char> input, ref int position)
-    {
-        while (position < input.Length && input[position] == ' ')
-        {
-            position++;
-        }
+        var minutesField = new MinutesField(reader.GetNextSegment());
+        var hoursField = new HoursField(reader.GetNextSegment());
 
-        var start = position;
-
-        while (position < input.Length && input[position] != ' ')
-        {
-            position++;
-        }
-
-        return input.Slice(start, position - start);
+        return new Cron(minutesField, hoursField);
     }
 
     public DateTimeOffset GetNextOccurrence(DateTimeOffset now)
     {
+        now = now.AddSeconds(1);
+
+        while (true)
+        {
+            if (!_minutesField.IsValid(now))
+            {
+                now = _minutesField.GetNext(now);
+                continue;
+            }
+
+            if (!_hoursField.IsValid(now))
+            {
+                now = _hoursField.GetNext(now);
+                continue;
+            }
+
+            break;
+        }
+
         return now;
     }
-    
-    
-    public struct CronField
+
+    private ref struct CronReader
     {
-    
+        private readonly ReadOnlySpan<char> _input;
+
+        public CronReader(ReadOnlySpan<char> input)
+        {
+            _input = input;
+        }
+
+        private int _position = 0;
+
+        public Segment GetNextSegment()
+        {
+            while (_position < _input.Length && _input[_position] == ' ')
+            {
+                _position++;
+            }
+
+            var start = _position;
+
+            while (_position < _input.Length && _input[_position] != ' ')
+            {
+                _position++;
+            }
+
+            return new Segment(_input.Slice(start, _position - start));
+        }
+    }
+
+    private ref struct Segment
+    {
+        public ReadOnlySpan<char> Value { get; }
+
+        public Segment(ReadOnlySpan<char> value)
+        {
+            Value = value;
+        }
+    }
+
+    private readonly ref struct MinutesField
+    {
+        private readonly Segment _segment;
+
+        public MinutesField(Segment segment)
+        {
+            _segment = segment;
+        }
+
+        public bool IsValid(DateTimeOffset date)
+        {
+            // 	* , - / 
+
+            var isAny = _segment.Value.IndexOf('*');
+            var isList = _segment.Value.IndexOf(',');
+            var isRange = _segment.Value.IndexOf('/');
+
+            return false;
+        }
+
+        public DateTimeOffset GetNext(DateTimeOffset date)
+        {
+            Span<Range> s = stackalloc Range[10];
+            var sd = _segment.Value.Split(s, ',');
+
+            return date;
+        }
+    }
+
+    private readonly ref struct HoursField
+    {
+        private readonly Segment _segment;
+
+        public HoursField(Segment segment)
+        {
+            _segment = segment;
+        }
+
+        public bool IsValid(DateTimeOffset now)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTimeOffset GetNext(DateTimeOffset now)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Backynet.Core/Abstraction/CronDefaults.cs
+++ Backynet.Core/Abstraction/CronDefaults.cs
@@ -8,29 +8,29 @@ public static class CronDefaults
 
     public static readonly Dictionary<int, int> MonthMap = new()
     {
-        { GetHashCode("JAN"), 0 },
-        { GetHashCode("FEB"), 1 },
-        { GetHashCode("MAR"), 2 },
-        { GetHashCode("APR"), 3 },
-        { GetHashCode("MAY"), 4 },
-        { GetHashCode("JUN"), 5 },
-        { GetHashCode("JUL"), 6 },
-        { GetHashCode("AUG"), 7 },
-        { GetHashCode("SEP"), 8 },
-        { GetHashCode("OCT"), 9 },
-        { GetHashCode("NOV"), 10 },
-        { GetHashCode("DEC"), 11 },
+        { GetHashCode("JAN"), 1 },
+        { GetHashCode("FEB"), 2 },
+        { GetHashCode("MAR"), 3 },
+        { GetHashCode("APR"), 4 },
+        { GetHashCode("MAY"), 5 },
+        { GetHashCode("JUN"), 6 },
+        { GetHashCode("JUL"), 7 },
+        { GetHashCode("AUG"), 8 },
+        { GetHashCode("SEP"), 9 },
+        { GetHashCode("OCT"), 10 },
+        { GetHashCode("NOV"), 11 },
+        { GetHashCode("DEC"), 12 },
     };
 
     public static readonly Dictionary<int, int> DayOfWeekMap = new()
     {
-        { GetHashCode("MON"), 0 },
-        { GetHashCode("TUE"), 1 },
-        { GetHashCode("WED"), 2 },
-        { GetHashCode("THU"), 3 },
-        { GetHashCode("FRI"), 4 },
-        { GetHashCode("SAT"), 5 },
-        { GetHashCode("SUN"), 6 },
+        { GetHashCode("MON"), 1 },
+        { GetHashCode("TUE"), 2 },
+        { GetHashCode("WED"), 3 },
+        { GetHashCode("THU"), 4 },
+        { GetHashCode("FRI"), 5 },
+        { GetHashCode("SAT"), 6 },
+        { GetHashCode("SUN"), 7 },
     };
     
     private static int GetMonth(ref ReadOnlySpan<char> input)
