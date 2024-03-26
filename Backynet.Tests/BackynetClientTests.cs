8352a72923f99883bd919a011f34bdbe62737a69
nickofc <damiancz112@gmail.com>
2024-03-26 20:13:56 +0100

    test: add test for enqueue action job

--- Backynet.Tests/BackynetClientTests.cs
+++ Backynet.Tests/BackynetClientTests.cs
@@ -5,8 +5,33 @@ namespace Backynet.Tests;
 
 public class BackynetClientTests
 {
+    public BackynetClientTests()
+    {
+        WasExecuted.Reset();
+    }
+
     [Fact]
-    public async Task Should_Execute_Job_When_Job_Was_Enqueued()
+    public async Task Should_Execute_Action_Job_When_Job_Was_Enqueued()
+    {
+        // todo: move to sut? 
+        using var timeout = new CancellationTokenSource();
+        timeout.CancelAfter(TimeSpan.FromSeconds(1000));
+
+        var factory = new NpgsqlConnectionFactory(TestContext.ConnectionString);
+        var serializer = new DefaultJsonSerializer();
+        var repository = new PostgreSqlRepository(factory, serializer);
+
+        var backynetServer = new BackynetServer(repository, new SimpleJobRunner(), new BackynetServerOptions());
+        await backynetServer.Start(CancellationToken.None);
+
+        var backynetClient = new BackynetClient(repository);
+        await backynetClient.EnqueueAsync(() => Console.WriteLine("hello world"), CancellationToken.None);
+
+        WasExecuted.Wait(timeout.Token);
+    }
+
+    [Fact]
+    public async Task Should_Execute_Func_Job_When_Job_Was_Enqueued()
     {
         using var timeout = new CancellationTokenSource();
         timeout.CancelAfter(TimeSpan.FromSeconds(1000));
