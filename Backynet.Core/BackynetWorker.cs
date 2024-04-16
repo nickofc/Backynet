f2cf747afd3f4705f39c252bc1841d5f8db2a196
nickofc <damiancz112@gmail.com>
2024-04-16 23:32:32 +0200

    feat: add heartbeat task to worker

--- Backynet.Core/BackynetWorker.cs
+++ Backynet.Core/BackynetWorker.cs
@@ -7,18 +7,24 @@ internal sealed class BackynetWorker : IBackynetWorker
     private readonly IJobRepository _jobRepository;
     private readonly IJobDescriptorExecutor _jobDescriptorExecutor;
     private readonly BackynetWorkerOptions _backynetWorkerOptions;
+    private readonly IControllerService _controllerService;
 
-    public BackynetWorker(IJobRepository jobRepository, IJobDescriptorExecutor jobDescriptorExecutor, BackynetWorkerOptions backynetWorkerOptions)
+    public BackynetWorker(IJobRepository jobRepository, IJobDescriptorExecutor jobDescriptorExecutor,
+        BackynetWorkerOptions backynetWorkerOptions, IControllerService controllerService)
     {
         _jobRepository = jobRepository;
         _jobDescriptorExecutor = jobDescriptorExecutor;
         _backynetWorkerOptions = backynetWorkerOptions;
+        _controllerService = controllerService;
     }
 
     public Task Start(CancellationToken cancellationToken)
     {
-        var task = StartCore(cancellationToken);
-        return task.IsCompleted ? task : Task.CompletedTask;
+        var heartbeatTask = _controllerService.Heartbeat(_backynetWorkerOptions.ServerName, cancellationToken);
+        var workerTask = StartCore(cancellationToken);
+
+        var combinedTasks = Task.WhenAll(heartbeatTask, workerTask);
+        return combinedTasks.IsCompleted ? combinedTasks : Task.CompletedTask;
     }
 
     private async Task StartCore(CancellationToken cancellationToken)
