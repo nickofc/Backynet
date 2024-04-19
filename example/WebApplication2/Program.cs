aedc37641008a10fae10b963207e29b8fd5310aa
nickofc <damiancz112@gmail.com>
2024-04-20 00:45:03 +0200

    feat: add example project

---
+++ example/WebApplication2/Program.cs
@@ -0,0 +1,43 @@
+using Backynet.Core.Abstraction;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/* add just backynet client */
+builder.Services.AddBackynetClient();
+
+/* add just backynet server */
+builder.Services.AddBackynetServer(options =>
+{
+    options.UseMaximumConcurrencyThreads(10);
+    options.UseServerName("fancy-server-name");
+
+    options.UsePostgreSql(options =>
+    {
+        options.UseConnectionString(Environment.GetEnvironmentVariable("BACKYNET_CONNECTION_STRING"));
+    });
+});
+
+var app = builder.Build();
+
+app.MapGet("/enqueue", async context =>
+{
+    var backynetClient = context.RequestServices.GetRequiredService<IBackynetClient>();
+    await backynetClient.EnqueueAsync(() => Func.DoWork());
+
+    Console.WriteLine("Job was enqueued");
+});
+
+app.Run();
+
+
+/* static class and static method is required.. for now */ 
+public static class Func
+{
+    public static async Task DoWork()
+    {
+        /* dependency injection is not yet supported :( */
+        Console.WriteLine("Executing job");
+        await Task.Delay(1000);
+        Console.WriteLine("Job executed");
+    }
+}
