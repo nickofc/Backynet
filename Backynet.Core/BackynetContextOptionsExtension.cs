77a2349d3c90ab8c0694d87f601d13b8fe5f6a18
nickofc <damiancz112@gmail.com>
2024-04-26 21:58:23 +0200

    feat: add backynet context options

--- Backynet.Core/BackynetContextOptionsExtension.cs
+++ Backynet.Core/BackynetContextOptionsExtension.cs
@@ -4,6 +4,40 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Backynet.Core;
 
+public interface IBackynetContextOptions
+{
+    IEnumerable<IBackynetContextOptionsExtension> Extensions { get; }
+
+    IBackynetContextOptionsExtension? FindExtension<TExtension>()
+        where TExtension : class, IBackynetContextOptionsExtension;
+}
+
+public abstract class BackynetContextOptions : IBackynetContextOptions
+{
+    private readonly IReadOnlyDictionary<Type, IBackynetContextOptionsExtension> _extensionsMap;
+
+    protected BackynetContextOptions()
+    {
+        _extensionsMap = new Dictionary<Type, IBackynetContextOptionsExtension>();
+        Extensions = _extensionsMap.Values;
+    }
+
+    public IEnumerable<IBackynetContextOptionsExtension> Extensions { get; } 
+
+    public IBackynetContextOptionsExtension? FindExtension<TExtension>() 
+        where TExtension : class, IBackynetContextOptionsExtension
+    {
+        return _extensionsMap.GetValueOrDefault(typeof(TExtension));
+    }
+    
+    public abstract IBackynetContextOptions WithExtension<TExtension>(TExtension extension) 
+        where TExtension : class, IBackynetContextOptionsExtension;
+}
+
+public class BackynetContextOptions<TContext>
+{
+}
+
 public class BackynetContextOptionsExtension : IBackynetContextOptionsExtension
 {
     public void ApplyServices(IServiceCollection services)
