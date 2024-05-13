a64d23e39a845bad613b81ea80f335c0337681a3
nickofc <damiancz112@gmail.com>
2024-05-13 22:54:16 +0200

    test: add test for deserialization

---
+++ Backynet.Tests/SerializerTests.cs
@@ -0,0 +1,24 @@
+﻿using Backynet.Abstraction;
+
+namespace Backynet.Tests;
+
+public class SerializerTests
+{
+    private readonly ISerializer _serializer = DefaultJsonSerializer.Instance;
+
+    [Fact]
+    public void Should_Serialize_With_Valid_Types()
+    {
+        var jobDescriptor = JobDescriptorFactory.Create(() => Run(1, 5));
+
+        var payload = _serializer.Serialize(jobDescriptor.Arguments);
+        var deserialized = _serializer.Deserialize<IArgument[]>(payload);
+
+        Assert.Equal(jobDescriptor.Arguments, deserialized);
+    }
+
+    private static Task Run(int argA, int argB)
+    {
+        return Task.CompletedTask;
+    }
+}   
