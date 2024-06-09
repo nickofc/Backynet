1f3e5830b411479226c261feafd06ef46315a221
nickofc <damiancz112@gmail.com>
2024-06-09 22:03:51 +0200

    test: add tests for job executor

--- Backynet.Tests/JobExecutorTests.cs
+++ Backynet.Tests/JobExecutorTests.cs
@@ -1,6 +1,8 @@
 using Backynet.Abstraction;
 using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NuGet.Frameworks;
 
 namespace Backynet.Tests;
 
@@ -19,42 +21,114 @@ public class JobExecutorTests
     }
 
     [Theory]
-    [InlineData(JobState.Unknown)]
-    [InlineData(JobState.Created)]
-    [InlineData(JobState.Enqueued)]
-    [InlineData(JobState.Scheduled)]
-    [InlineData(JobState.Processing)]
-    [InlineData(JobState.Failed)]
-    [InlineData(JobState.Succeeded)]
-    [InlineData(JobState.Canceled)]
-    [InlineData(JobState.Deleted)]
-    public async Task Should_Throw_Invalid_Operation_Exception_When_Trying_Execute_Job_In_Invalid_State(JobState jobState)
+    [InlineData(JobState.Unknown, true)]
+    [InlineData(JobState.Created, false)]
+    [InlineData(JobState.Enqueued, false)]
+    [InlineData(JobState.Scheduled, false)]
+    [InlineData(JobState.Processing, true)]
+    [InlineData(JobState.Failed, true)]
+    [InlineData(JobState.Succeeded, true)]
+    [InlineData(JobState.Canceled, true)]
+    [InlineData(JobState.Deleted, true)]
+    public async Task Should_Throw_Invalid_Operation_Exception_When_Trying_Execute_Job_In_Invalid_State(JobState jobState, bool shouldThrow)
     {
         var jobDescriptor = JobDescriptorFactory.Create(() => FakeMethod());
         var job = JobFactory.Create(jobDescriptor);
 
         job.JobState = jobState;
-        
-        await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+
+        if (shouldThrow)
+        {
+            await Assert.ThrowsAsync<InvalidOperationException>(async () => { await _jobExecutor.Execute(job); });
+        }
+        else
         {
             await _jobExecutor.Execute(job);
-        });
+        }
     }
-    
+
+    [Fact]
+    public async Task Should_Execute_Job_When_JobState_Is_Created_And_Date_Is_From_Past()
+    {
+        var jobDescriptor = JobDescriptorFactory.Create(() => FakeMethod());
+        var job = JobFactory.Create(jobDescriptor);
+        var now = DateTimeOffset.UtcNow;
+
+        job.JobState = JobState.Created;
+        job.NextOccurrenceAt = now;
+
+        await _jobExecutor.Execute(job);
+
+        Assert.Equal(JobState.Succeeded, job.JobState);
+    }
+
+    [Fact]
+    public async Task Should_Schedule_Job_When_JobState_Is_Created_And_Date_Is_From_Future()
+    {
+        var jobDescriptor = JobDescriptorFactory.Create(() => FakeMethod());
+        var job = JobFactory.Create(jobDescriptor);
+        var now = DateTimeOffset.UtcNow;
+
+        job.JobState = JobState.Created;
+        job.NextOccurrenceAt = now.AddDays(1);
+
+        await _jobExecutor.Execute(job);
+
+        Assert.Equal(JobState.Scheduled, job.JobState);
+    }
+
+    [Fact]
+    public async Task Should_Change_State_To_Succeeded_When_Job_State_Is_Created_And_Next_Occurrence_At_Is_Null()
+    {
+        var jobDescriptor = JobDescriptorFactory.Create(() => FakeMethod());
+        var job = JobFactory.Create(jobDescriptor);
+
+        job.JobState = JobState.Created;
+        job.NextOccurrenceAt = null;
+
+        await _jobExecutor.Execute(job);
+
+        Assert.Equal(JobState.Succeeded, job.JobState);
+    }
+
+    [Fact]
+    public async Task Should_Change_State_To_Failed_When_Job_Execution_Throws_Exception()
+    {
+        var jobDescriptor = JobDescriptorFactory.Create(() => FakeMethodThatThrowsException());
+        var job = JobFactory.Create(jobDescriptor);
+
+        _jobDescriptorExecutor
+            .Execute(Arg.Any<IJobDescriptor>(), Arg.Any<CancellationToken>())
+            .ThrowsAsync<JobDescriptorExecutorException>();
+
+        job.JobState = JobState.Created;
+        job.NextOccurrenceAt = null;
+
+        await _jobExecutor.Execute(job);
+
+        Assert.Equal(JobState.Failed, job.JobState);
+        Assert.Single(job.Errors);
+    }
+
     [Fact]
     public async Task Should_Cancel()
     {
         _jobRepository.Update(Arg.Any<Guid>(), Arg.Any<Job>()).Returns(true);
-        
+
         var cancellationTokenSource = new CancellationTokenSource(0);
 
         var jobDescriptor = JobDescriptorFactory.Create(() => FakeMethod());
         var job = JobFactory.Create(jobDescriptor);
-        
+
         await _jobExecutor.Execute(job, cancellationTokenSource.Token);
     }
 
     public static void FakeMethod()
     {
+    }
+
+    public static void FakeMethodThatThrowsException()
+    {
+        throw new Exception(nameof(FakeMethodThatThrowsException));
     }
 }
